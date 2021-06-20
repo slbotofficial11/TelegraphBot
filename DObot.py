5ad472ac22afea0371170e75e40f21464c2a9cfd
SL Bots <84837445+slbotofficial11@users.noreply.github.com>
2021-06-20 21:07:28 +0530

    Update DObot.py

--- DObot.py
+++ DObot.py
@@ -19,19 +19,19 @@ async def start(client, message):
                chat_id=message.chat.id,
                text="""<b>Hello 👋 there! I can upload photos,videos & gif animations to telegraph and provide you the link.
  Send me /help for more info.
-A project by @dihanofficial 🇱🇰
+A project by @slhitbotofficial
 ✅ Join our bots channel and group
-@dihan_official | @Dihanofficial
-</b>""",     
+@slhitbotofficial | @slhitofficialbot
+</b>""",
                              reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
                                             "✍️Help👨‍💻", callback_data="help"),
                                         InlineKeyboardButton(
-                                            "Channel", url="https://t.me/dihanofficial")
+                                            "Channel", url="https://t.me/slhitbotofficial")
                                     ],[
                                       InlineKeyboardButton(
-                                            "Developer", url="https://t.me/dihanrandila")
+                                            "Developer", url="https://t.me/darkridersslk")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -45,7 +45,7 @@ async def help(client, message):
                chat_id=message.chat.id,
                text="""<b>Telegraph Bot Help!
 Just send a photo or video less than 5mb file size, I'll upload it to telegraph.
-~ @dihan_official</b>""",
+~ @slhitbotofficial</b>""",
         reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
@@ -54,7 +54,7 @@ Just send a photo or video less than 5mb file size, I'll upload it to telegraph.
                                             "About", callback_data="about"),
                                   ],[
                                         InlineKeyboardButton(
-                                            "Updates", url="https://t.medihanofficial")
+                                            "Updates", url="https://t.me/slhitbotofficial")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -66,19 +66,19 @@ async def about(client, message):
         await DObot.send_message(
                chat_id=message.chat.id,
                text="""<b>About Telegraph Bot!</b>
-<b> Developer:</b> <a href="https://t.me/Dihanrandila">Dihan Randila 🇱🇰</a>
+<b> Developer:</b> <a href="https://t.me/darkridersslk">Sadew Jayasekara </a>
 
-<b> Support:</b> <a href="https://t.me/dihan_Official">Dihan Official Support</a>
+<b> Support:</b> <a href="https://t.me/slhitofficialbot">🇱🇰SL BOTs squad</a>
 
 <b> Library:</b> <a href="https://github.com/pyrogram/pyrogram">Pyrogram</a>
 
-<b>~ @dihanofficial</b>""",
+<b>~ @slhitbotofficial</b>""",
      reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
                                             "Back", callback_data="help"),
                                         InlineKeyboardButton(
-                                            "Support Group", url="https://t.me/dihan_Official")
+                                            "Support Group", url="https://t.me/slhitofficialbot")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -110,7 +110,7 @@ async def telegraphvid(client, message):
     except:
         await msg.edit_text("Video size should be less than 5mb!") 
     else:
-        await msg.edit_text(f'**📤 Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @dihan_official*',
+        await msg.edit_text(f'**📤 Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @slhitofficialbot*',
             disable_web_page_preview=True,
         )
     finally:
@@ -126,7 +126,7 @@ async def telegraphgif(client, message):
     except:
         await msg.edit_text("Gif size should be less than 5mb!") 
     else:
-        await msg.edit_text(f'**📤 Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @dihan_official**',
+        await msg.edit_text(f'**📤 Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @slhitofficialbot**',
             disable_web_page_preview=True,
         )
     finally:
@@ -148,7 +148,7 @@ async def button(bot, update):
 print(
     """
 Bot Started!
-Join @dihan_official
+Join @slhitofficialbot
 """
 )
 
